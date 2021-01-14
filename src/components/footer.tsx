b8a917dfca524a0420b86c9e23aa35cd1da3e2aa
pablohariel <pablohhdr@gmail.com>
2021-01-14 03:45:32 -0300

    Home Page Responsiveness

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/Footer.module.css'
+
+const Footer: React.FC = () => {
+    return (
+        <div className={styles.main}>
+            <h1 className={styles.title}>Contato</h1>
+            <h2 className={styles.email}>pabloharie@gmail.com</h2>
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from "next";
 import LanguageSelector from '../components/languageSelector';
 import Header from '../components/header';
+import Footer from '../components/footer';
 import styles from '../styles/pages/Home.module.css';
-
+import { IoOpenOutline } from "react-icons/io5";
 import Logo from '../assets/logo.svg'
+import colors from '../config/colors'
 
 interface Props {
   pageTitle: string,
@@ -16,16 +18,35 @@ interface Props {
 
 const Blog: React.FC<Props> = ({ posts, pageTitle, pageDescription }) => {
   return (
-    <div className={styles.container}> 
+    <div className={styles.main}> 
       <Header />
-      <div className={styles.posts}> 
-        {posts && posts.map(post => (
-          <div key={Math.random()}>
-            <h1>{post.title}</h1>
-            <p>{post.content}</p>
+      <div className={styles.body}> 
+        <div className={styles.info}>
+          <h1 className={styles.name}>Pablo Hariel</h1>
+          <p className={styles.description}>Programador Web full stack, com foco em tecnologias modernas como React.js e Node.js</p>
+        </div>
+        <div className={styles.btns}>
+          <div className={styles.btn}>
+            <p className={styles.btn_text}>Projetos</p> 
+            {/* <div className={styles.btn_icon}>
+              <IoOpenOutline size='100%' color={colors.color_secondary} />
+            </div> */}
           </div>
-        ))}
+          <div className={styles.btn}>
+            <p className={styles.btn_text}>Experiencia</p>
+            {/* <div className={styles.btn_icon}>
+              <IoOpenOutline size='100%' color={colors.color_secondary} />
+            </div> */}
+          </div>
+          <div className={styles.btn}>
+            <p className={styles.btn_text}>Sobre</p>
+            {/* <div className={styles.btn_icon}>
+              <IoOpenOutline size='100%' color={colors.color_secondary} />
+            </div> */}
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   )
 }
